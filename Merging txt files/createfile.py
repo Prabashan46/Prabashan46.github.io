1b617c9e752c6ce519918be390881b04b7488a6f
krprlk <krishnamoorthy.prabashan@ifs.com>
2024-02-24 13:18:24 +0530

    jkhjkjkjk

--- Merging txt files/createfile.py
+++ Merging txt files/createfile.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 # List of input file names
 input_files = ['cones.txt', 'cones (1).txt', 'cones (2).txt', 'cones (3).txt', 'cones (4).txt']
@@ -8,11 +9,12 @@ merged_cones = []
 
 # Loop through each input file
 for file_name in input_files:
-    with open(file_name, 'r') as file:
-        # Read JSON data from the file
-        data = json.load(file)
-        # Extend the merged_cones list with data from the current file
-        merged_cones.extend(data)
+    if os.path.exists(file_name):
+        with open(file_name, 'r') as file:
+            # Read JSON data from the file
+            data = json.load(file)
+            # Extend the merged_cones list with data from the current file
+            merged_cones.extend(data)
 
 # Write the merged data to a new file
 with open('merged_cones.txt', 'w') as output_file:
